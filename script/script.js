a055623a5e003958618c7780102d9baaf0765cb6
mehmetcetins <mehmetcetins@hotmail.com>
2017-12-14 12:23:23 +0300

    vize ortalaması hesaplama eklendi

--- script/script.js
+++ script/script.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-
+    var arr = ["BMB","FZK","LCI","MAT","TBT","TDI","YDI"];
+    $(".submit").click(function(){
+        var notlar = [];
+        var katsayilar = [];
+        $.each(arr,function(i,val){
+            value = $("input[name='"+val+"']").val();
+            if(  value == 0 ){
+                notlar.push(0);
+                katsayilar.push($("input[name='"+val+"']").attr("kredi"));
+            }
+            else {
+                notlar.push(parseFloat(value));
+                katsayilar.push(parseInt($("input[name='"+val+"']").attr("kredi")));
+            }
+        })
+        k=0;
+        c=0;
+        for(i = 0;i<notlar.length;i++){
+            c += notlar[i]*katsayilar[i];
+            k += katsayilar[i];
+        }
+        $(".vize td:eq(1)").text(c/k);
+        $(".vize td:eq(2)").text((c/k)*0.3);
+        return(false);
+    })
 })
